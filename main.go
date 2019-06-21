7041364b6e3315729a6f7859e0262a749f636145
Rodion Artyukhin <artyukhin.rodion@gmail.com>
2019-06-21 22:30:38 +0300

    Add new handlers and routes

--- main.go
+++ main.go
@@ -5,13 +5,31 @@ import (
 	"net/http"
 )
 
+// Функция home отображает сообщение на главной странице
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Функция showSnippet отображает конкретный сниппет
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Функция createSnippet создает новый сниппет
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
