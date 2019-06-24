8076041cb96a7230505b87baace6c1488e6fa7b9
Rodion Artyukhin <artyukhin.rodion@gmail.com>
2019-06-24 18:21:56 +0300

    Add command-line flag for setting up the port address

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Значение флага будет храниться в переменной addr в runtime
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Парсит значение флага из командной строки
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
